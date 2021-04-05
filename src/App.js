fcb7299d4974762d6cade42c401d52dbc557ed4f
MarcoCarpo <marco.carpo95@gmail.com>
2021-04-05 16:10:06 +0200

    set react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home, Error, Country } from "./pages";
+import { Navbar } from "./components";
 
 function App() {
-  return <main className="app"></main>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/countries/:id">
+          <Country />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import AppProvider from "./context";
 
 import reportWebVitals from "./reportWebVitals";
 
-
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
