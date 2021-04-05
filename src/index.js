a16ad0736d0cbfd2dba68b8aa17f21be233b4caf
MarcoCarpo <marco.carpo95@gmail.com>
2021-04-05 15:17:57 +0200

    add some notes and add scss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import "./scss/style.scss";
+
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import AppContext from "./context";
+
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
