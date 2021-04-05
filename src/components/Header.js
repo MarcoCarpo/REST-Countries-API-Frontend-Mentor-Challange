b94813b82ab0f07e5f9275558f6b1ca01e2e8641
MarcoCarpo <marco.carpo95@gmail.com>
2021-04-05 17:32:14 +0200

    style navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,11 @@ import { FaMoon, FaRegMoon } from "react-icons/fa";
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__content">
-        <h1 className="header__title">where in the world?</h1>
-        <button className="header__dark-mode">
-          <FaMoon />
-          darkmode
-        </button>
-      </div>
+      <h1 className="header__title">Where in the world?</h1>
+      <button className="header__dark-mode">
+        <FaMoon />
+        dark mode
+      </button>
     </header>
   );
 };
